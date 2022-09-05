033064d3e2ed018580ac39a30af681a2b70ad188
Zeeshan Paracha <zparacha82@gmail.com>
2022-09-06 00:59:28 +0500

    cors

--- src/config/server.js
+++ src/config/server.js
@@ -4,17 +4,14 @@ const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
-
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Api is running on port ${port}`));
